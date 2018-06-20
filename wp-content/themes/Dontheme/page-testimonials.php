1acc2148e8f7fe196824e278ed99743179d4462a
malinek <malin.ekelund@gmail.com>
2018-06-20 17:23:54 +0200

    styling and content testimonials page

--- wp-content/themes/Dontheme/page-testimonials.php
+++ wp-content/themes/Dontheme/page-testimonials.php
@@ -1,28 +1,28 @@
 <?php get_header(); ?>
 
  
-  <div class="header" style="background-image: url(<?php echo get_theme_file_uri('/images/dandelions.jpg') ?>);">
+  <div class="header" style="background-image: url(<?php echo get_theme_file_uri('/images/testimonials-header2.jpg') ?>);">
     <div class="header-headline">
       <h1>Testimonials</h1>
     </div>
   </div>
  
 <div class="container">
+  
   <div class="row testimonials-box">
     <div class="col-sm-4 image-column">
       <img class="testimonials-image" src="<?php echo get_template_directory_uri() . '/images/cathy.jpg'; ?>" />
     </div>
     <div class="col-sm-8">
       <div class="quotation-mark">
-        <img class="testimonials-icon" src="<?php echo get_template_directory_uri() . '/images/quotationmark.png'; ?>" />
+        <img class="testimonials-icon" src="<?php echo get_template_directory_uri() . '/images/quotationmark2.png'; ?>" />
       </div>
       <div class="testimonial-content">
         <p>I have been working with Donald now consistently for over 15 years. During this time I have gone though such a great metamorphose that I feel like I can now say I feel love and joy pretty much most of the time and I have created a really wonderful life for myself. I teach yoga, offer sessions in this work, make films and travel the world all while being a mama. Donald has been my guide & my friend and helped me through the darkest days of my spiritual journey and taught me how to embrace it all. He is a pioneer, a trailblazer, a risk taker and the brightest channel of light I have ever had the privilege of meeting. To work with him is to enter into an entirely new vibration of light.</p>
       </div>
-      <h4>Cathy Pearson<br>Yoga Teacher & Film Director<br>www.yogadeep.com</h4>
+      <h4>Cathy Pearson<br>Yoga Teacher and Film Director<br><a href="http://www.yogadeep.com/">www.yogadeep.com</a></h4>
     </div>
   </div><!-- end row -->
-
 
   <div class="row testimonials-box">
     <div class="col-sm-4">
@@ -30,7 +30,7 @@
     </div>
     <div class="col-sm-8">
       <div class="quotation-mark">
-        <img class="testimonials-icon" src="<?php echo get_template_directory_uri() . '/images/quotationmark.png'; ?>" />
+        <img class="testimonials-icon" src="<?php echo get_template_directory_uri() . '/images/quotationmark2.png'; ?>" />
       </div>
       <div class="testimonial-content">
         <p>Donald reconnected me with the divine within. His gift of bringing through the unconditional love necessary to melt away the emotional, mental and physical blocks to our divine self is beyond works. Experiencing his work helped me remember what I came to do. He is Unconditional Love.</p>
@@ -38,25 +38,187 @@
       <h4>Brett Cotter<br>Energy Worker, New York</h4>
     </div>
   </div><!-- end row -->
-</div><!--end container -->
 
-
-
-
-<!--
-
-<div class="container">
-  <div class="row">
-  <div class="col-sm-6 col-md-4">
-    <div class="thumbnail" style="background-image: url(<?php echo get_theme_file_uri('/images/cathy.jpg') ?>);">
-      <div class="caption">
-        <h3>Thumbnail label</h3>
-        <p>I have been working with Donald now consistently for over 15 years. During this time I have gone though such a great metamorphose that I feel like I can now say I feel love and joy pretty much most of the time and I have created a really wonderful life for myself. I teach yoga, offer sessions in this work,make films and travel the world all while being a mama. Donald has been my guide & my friend and helped me through the darkest days of my spiritual journey and taught me how to embrace it all. He is a pioneer, a trailblazer, a risk taker and the brightest Channel of light I have ever had the privilege of meeting. To work with him is to enter into an entirely new vibration of light. www.yogadeep.com. Cathy Pearson, Yoga Teacher & Film Director.</p>
-      </div>
+  <div class="row testimonials-box">
+    <div class="col-sm-4 image-column">
+      <img class="testimonials-image" src="<?php echo get_template_directory_uri() . '/images/julie.jpg'; ?>" />
     </div>
-  </div>
-</div>
-</div>end container --> 
+    <div class="col-sm-8">
+      <div class="quotation-mark">
+        <img class="testimonials-icon" src="<?php echo get_template_directory_uri() . '/images/quotationmark2.png'; ?>" />
+      </div>
+      <div class="testimonial-content">
+        <p>Psychotherapy works with the mind and thoughts.  It  is very hard to alter thought patterns by using more thoughts.  There is a risk of intensifying the split between mind and body, making a more ‘clever’ more presentable Ego, that will pass for ‘normal’ in our dysfunctional world. Donald’s work is so different, it’s the opposite.  He works with the Energy, and with feeling in the body, which allows any patterns we have taken on to be released.   The Light, the Love is the foundation for his work, it is always there, it is the backdrop, the theme, the means and the method.  There is no ‘working towards’ the Light, the Light is the starting place.</p>
+      </div>
+      <h4>Julie Forsey<br>Medical Doctor Wellington, New Zealand</h4>
+    </div>
+  </div><!-- end row -->
 
+  <div class="row testimonials-box">
+    <div class="col-sm-4 image-column">
+      <img class="testimonials-image" src="<?php echo get_template_directory_uri() . '/images/Mary.jpg'; ?>" />
+    </div>
+    <div class="col-sm-8">
+      <div class="quotation-mark">
+        <img class="testimonials-icon" src="<?php echo get_template_directory_uri() . '/images/quotationmark2.png'; ?>" />
+      </div>
+      <div class="testimonial-content">
+        <p>Working with Donald in a workshop or private session is truly the absolute best thing you can do for yourself and your soul! Donald, incredibly intuitive and well-trained has an absolutely awesome gift for channeling pure Spiritual energy… and this gift is very strong! As a healer working in the field for many years, I have never met anyone more able to bring through the love and energy of the Divine Spirit. This energy heals… it transforms all of our old programming and belief systems… it brings through love, joy, passion, excitement and abundance… and most importantly it allows for total connection with the Divine… there is no-one more able to do this work. Working with Donald is truly a gift!</p>
+      </div>
+      <h4>Mary Costello<br>Legal Administrator, Long Island, New York.</h4>
+    </div>
+  </div><!-- end row -->
+
+  <div class="row testimonials-box">
+    <div class="col-sm-4 image-column">
+      <img class="testimonials-image" src="<?php echo get_template_directory_uri() . '/images/Derek.jpg'; ?>" />
+    </div>
+    <div class="col-sm-8">
+      <div class="quotation-mark">
+        <img class="testimonials-icon" src="<?php echo get_template_directory_uri() . '/images/quotationmark2.png'; ?>" />
+      </div>
+      <div class="testimonial-content">
+        <p>This process is truly an adventure! It has challenged and stretched me. It has opened me and it has guided me. The people, the love and the energy in these groups have inspired me, embraced me, supported me, expanded me, liberated me and loved me. In these groups I feel I have come home to a way of being and relating to others that my heart has always longed for. I recommend this way for those who share this longing!</p>
+      </div>
+      <h4>Derek Plunkett<br>Healer and Practitioner, Dublin, Ireland.</h4>
+    </div>
+  </div><!-- end row -->
+
+  <div class="row testimonials-box">
+    <div class="col-sm-4 image-column">
+      <img class="testimonials-image" src="<?php echo get_template_directory_uri() . '/images/Ted.jpg'; ?>" />
+    </div>
+    <div class="col-sm-8">
+      <div class="quotation-mark">
+        <img class="testimonials-icon" src="<?php echo get_template_directory_uri() . '/images/quotationmark2.png'; ?>" />
+      </div>
+      <div class="testimonial-content">
+        <p>I chose Donald as my teacher.  I chose to trust him and others around him above the parts of myself that I could not be clear about. 20 years of exploring my own self-consciousness does not diminish this trust, the opposite.  The more time he has been in my life the more I move toward recognizing his evolving mastery as an individual teacher and role model.  From the first moment I entered his workshop I recognized he was teaching people to get to where he was and I wanted to go. His work has changed my life in every possible way and I extremely grateful for that. He has never created any reliance on him only ever guided me back to my own truth, growth and knowingness.  After 15 years I can say his work is not only the cutting edge of where any individual or humanity can go, he himself is a model of it. </p>
+      </div>
+      <h4>Ted Kirby<br>Therapist, Panama</h4>
+    </div>
+  </div><!-- end row -->
+
+  <div class="row testimonials-box">
+    <div class="col-sm-4 image-column">
+      <img class="testimonials-image" src="<?php echo get_template_directory_uri() . '/images/Gretta.jpg'; ?>" />
+    </div>
+    <div class="col-sm-8">
+      <div class="quotation-mark">
+        <img class="testimonials-icon" src="<?php echo get_template_directory_uri() . '/images/quotationmark2.png'; ?>" />
+      </div>
+      <div class="testimonial-content">
+        <p>For 10 years before meeting Donald I suffered from chronic back pain. In the first workshop with Donald he released a deep cellular memory within me that was causing it. Since then I have had no need for regular pain killers. Donald's channel and connection with Spirit is truly awesome and I have witnessed profound changes within me and others. Thank you Donald.</p>
+      </div>
+      <h4>Gretta Barry<br>Ennis, Ireland</h4>
+    </div>
+  </div><!-- end row -->
+
+  <div class="row testimonials-box">
+    <div class="col-sm-4 image-column">
+      <img class="testimonials-image" src="<?php echo get_template_directory_uri() . '/images/Johanna.jpg'; ?>" />
+    </div>
+    <div class="col-sm-8">
+      <div class="quotation-mark">
+        <img class="testimonials-icon" src="<?php echo get_template_directory_uri() . '/images/quotationmark2.png'; ?>" />
+      </div>
+      <div class="testimonial-content">
+        <p>Donald Hanson is a truly gifted and very experienced teacher. I have known him for twenty years and seen miracle after miracle taking place in these circles. This is very powerful work for those who are willing to be vulnerable, take responsibility for their feelings and expand beyond the ego mind into higher vibrational reality. Donald transmits pure light and healing energies, which has abled me to go through a profound purification process. The work teaches you about energy, how the intelligence of the universe operates and how to have a relationship with it. Your life will not be the same. I am so grateful for Donald and the structure coming through him.</p>
+      </div>
+      <h4>Johanna Harmala<br>Owner of Lake Arenal Retreat Center, Costa Rica</h4>
+    </div>
+  </div><!-- end row -->
+
+  <div class="row testimonials-box">
+    <div class="col-sm-4 image-column">
+      <img class="testimonials-image" src="<?php echo get_template_directory_uri() . '/images/lady.jpg'; ?>" />
+    </div>
+    <div class="col-sm-8">
+      <div class="quotation-mark">
+        <img class="testimonials-icon" src="<?php echo get_template_directory_uri() . '/images/quotationmark2.png'; ?>" />
+      </div>
+      <div class="testimonial-content">
+        <p>I began working with Donald in the 90's . It was a wonderful process for me. I was helped through the most difficult challenges of my life in the most gentle, playful, non judgmental, loving and safe space. It was an honor to host Donald at times and also to travel in Sedona and Ghana with him. I learned and healed so much and so did one of my sons. Much water has flown under the bridge since then and last year it was very exciting for me to reconnect with Donald and Johanna at her beautiful retreat centre in Costa Rica. This time was very different in that I was able to take ownership of my own shadow and really feel in my body the pain of how it feels to "make it about the other." What a revelation...So, here we go again... Back in kindergarten and ready for the next adventures with Spirit and old friends. Change and new beginnings - always exciting. Here's to 2017. Life is good.</p>
+      </div>
+      <h4></h4>
+    </div>
+  </div><!-- end row -->
+
+  <div class="row testimonials-box">
+    <div class="col-sm-4 image-column">
+      <img class="testimonials-image" src="<?php echo get_template_directory_uri() . '/images/Tom.jpg'; ?>" />
+    </div>
+    <div class="col-sm-8">
+      <div class="quotation-mark">
+        <img class="testimonials-icon" src="<?php echo get_template_directory_uri() . '/images/quotationmark2.png'; ?>" />
+      </div>
+      <div class="testimonial-content">
+        <p>The work I have been doing with Donald over the last year has totally changed my life. My connection with Spirit is getting stronger all the time. I am happier and more joyful and I live much more in the present. My relationships both personal and business have improved enormously. Because I am more open to new possibilities and trusting more, prosperity is flowing in my life.</p>
+      </div>
+      <h4>Tom Galligan<br>Bar Proprietor, Dublin, Ireland</h4>
+    </div>
+  </div><!-- end row -->
+
+  <div class="row testimonials-box">
+    <div class="col-sm-4">
+      <img class="testimonials-image" src="<?php echo get_template_directory_uri() . '/images/Bridget.jpg'; ?>" />
+    </div>
+    <div class="col-sm-8">
+      <div class="quotation-mark">
+        <img class="testimonials-icon" src="<?php echo get_template_directory_uri() . '/images/quotationmark2.png'; ?>" />
+      </div>
+      <div class="testimonial-content">
+        <p>Donald has a rare gift to share. He has helped me get in touch with and release blocks in my energy flow as well as mental and emotional bodies. He works in a clear and direct way to assist deep and profound changes that are lasting.</p>
+      </div>
+      <h4>Bridget Van Block<br>M.A. Psychology, Sedona, AZ.</h4>
+    </div>
+  </div><!-- end row -->
+
+  <div class="row testimonials-box">
+    <div class="col-sm-4">
+      <img class="testimonials-image" src="<?php echo get_template_directory_uri() . '/images/Dawn.jpg'; ?>" />
+    </div>
+    <div class="col-sm-8">
+      <div class="quotation-mark">
+        <img class="testimonials-icon" src="<?php echo get_template_directory_uri() . '/images/quotationmark2.png'; ?>" />
+      </div>
+      <div class="testimonial-content">
+        <p>Donald Hanson's work taps into such a deeply spiritual, profoundly healing energy that mere words cannot possibly capture the wonder and scope of its essence. He is coming from such a boundless, generous, limitless place of acceptance, love and presence. My body released pain and trauma that had been lodged in my cellular makeup for over a decade. His workshop has been a catalyst for my unfolding into my beauty and power. He is a true healer and benevolent teacher filled with joy and light.</p>
+      </div>
+      <h4>Dawn Colombo<br>Holistic Service Reviewer, Tendu Magazine, New York.</h4>
+    </div>
+  </div><!-- end row -->
+
+  <div class="row testimonials-box">
+    <div class="col-sm-4">
+      
+    </div>
+    <div class="col-sm-8">
+      <div class="quotation-mark">
+        <img class="testimonials-icon" src="<?php echo get_template_directory_uri() . '/images/quotationmark2.png'; ?>" />
+      </div>
+      <div class="testimonial-content">
+        <p>Dear Donald, This is to appreciate your memorable visit to my school on Wednesday 23rd September 2003, and to thank you for donating cash and clothing to the children. Your kind gesture is not only inspiring but also supplements in practical terms the basic lessons of giving and receiving, which we impart to the children. Most importantly it is a motivating factor in our enrollment drive. You have opened your heart and your hand to the needy and deprived children. May the Holy Spirit uphold, preserve and fulfill your desires. May success crown all your untiring efforts. Thanks, Your Friend, Emmanuel Eghan</p>
+      </div>
+      <h4>Emmanuel Eghan<br>Director, Christian Home Institute, Kokrobite, Accra, Ghana, Africa.</h4>
+    </div>
+  </div><!-- end row -->
+
+  <div class="row testimonials-box">
+    <div class="col-sm-4">
+      
+    </div>
+    <div class="col-sm-8">
+      <div class="quotation-mark">
+        <img class="testimonials-icon" src="<?php echo get_template_directory_uri() . '/images/quotationmark2.png'; ?>" />
+      </div>
+      <div class="testimonial-content">
+        <p>On my first encounter with Donald's work, a 25 year struggle with the crippling pain of migraine simply vanished. Since then my life has changed immeasurably. I have an inner sense of peace and purpose I could never before have imagined. Donald is a great master and a wonderful gift to us all from Spirit.</p>
+      </div>
+      <h4>Judy<br>Tourist Industry, Northern Ireland.</h4>
+    </div>
+  </div><!-- end row -->
+
+</div><!--end container -->
 
 <?php get_footer(); ?>
