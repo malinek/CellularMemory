2cf61155eb49456caac793b2f014336bb9f9e64b
malinek <malin.ekelund@gmail.com>
2018-08-02 12:57:22 +0200

    styling testimonials page

--- wp-content/themes/Dontheme/js/scripts.js
+++ wp-content/themes/Dontheme/js/scripts.js
@@ -14,9 +14,10 @@
   });
 
 
+  $(".myParallax").parallax();
 
-/*
-  $('.site-nav li > .sub-menu').parent().hover(function() {
+
+  /*$('.site-nav li > .sub-menu').parent().hover(function() {
   var submenu = $(this).children('.sub-menu');
   if ( $(submenu).is(':hidden') ) {
     $(submenu).slideDown(500);
@@ -24,8 +25,29 @@
     $(submenu).slideUp(500);
   }
 
-  });
-*/
+  }); */
+
+  /*$(window).scroll(function(e) {
+    parallax();
+  })
+
+
+  function parallax() {
+    var scroll = $(window).scrollTop();
+    var screenHeight = $(window).height();
+
+    $('.parallax').each(function() {
+      var offset = $(this).offset().top;
+      var distanceFromBottom = offset - scroll - screenHeight
+      
+      if (offset > screenHeight && offset) {
+        $(this).css('background-position', 'center ' + (( distanceFromBottom  ) * 0.5) +'px');
+      } else {
+        $(this).css('background-position', 'center ' + (( -scroll ) * 0.5) + 'px');
+      }
+    })
+  } */
+
 
 // WRAPPER FOR ALL CONTENT
 })(jQuery);
@@ -34,7 +56,7 @@
 /* PARALLAX EFFECT NO JQUERY
 let p = document.getElementsByClassName('parallax')[0];
 window.onscroll = function(){
-  let scrollY = window.scrollY;
+  let scrollY = (window.scrollY > 70);
   p.style.backgroundPosition = 'center ' + (scrollY*0.4) + 'px';
   
 };﻿ */
