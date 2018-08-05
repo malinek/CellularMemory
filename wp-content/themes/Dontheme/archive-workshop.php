3fde25719cea6db24f659eff3586d3e110353a36
malinek <malin.ekelund@gmail.com>
2018-08-05 16:41:54 +0200

    build workshops single page

--- wp-content/themes/Dontheme/archive-workshop.php
+++ wp-content/themes/Dontheme/archive-workshop.php
@@ -11,36 +11,36 @@
 <div class="container container-workshops">
  
   <?php
-  while(have_posts()) {
-    the_post(); ?>
-    <div class="row workshops-box">
-      
-      <div class="col-sm-12 date-column">
-        <img class="calender-icon" src="<?php echo get_template_directory_uri() . '/images/calender-icon2.png'; ?>" />
-        <h3 class="start-date"><?php
-        $workshopStartDate = new DateTime(get_field('workshop_start_date'));
-        echo $workshopStartDate->format('j F Y')
-        ?> -<br> <?php
-        $workshopEndDate = new DateTime(get_field('workshop_end_date'));
-        echo $workshopEndDate->format('j F Y')
-        ?></h3>
-      </div>
-     
-      <div class="col-sm-12 content-workshops">
-        <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-        <p><?php the_content(); ?></p>
-        <button type="button" class="green-button"><a href="<?php the_permalink(); ?>">Read more</a></button>
-      </div>
-     
-    </div><!-- end row -->
+    while(have_posts()) {
+      the_post();
+  ?>
 
-   <?php }
-   ?> 
+  <div class="row workshops-box">
+    
+    <div class="col-sm-12 date-column">
+      <img class="calender-icon" src="<?php echo get_template_directory_uri() . '/images/calender-icon2.png'; ?>" />
+      <h3 class="start-date"><?php
+      $workshopStartDate = new DateTime(get_field('workshop_start_date'));
+      echo $workshopStartDate->format('j F Y')
+      ?> -<br> <?php
+      $workshopEndDate = new DateTime(get_field('workshop_end_date'));
+      echo $workshopEndDate->format('j F Y')
+      ?></h3>
+    </div>
+   
+    <div class="col-sm-12 content-workshops">
+      <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+      <p><?php the_field('further_information'); ?></p>
+      <button type="button" class="green-button"><a href="<?php the_permalink(); ?>">Read more</a></button>
+    </div>
+   
+  </div><!-- end row workshops-box-->
+
+  <?php }
+  ?> 
 
   <?php echo paginate_links(); ?>
 
   </div><!-- end row workshop-box -->
-
-</div><!-- end container-->
 
 <?php get_footer(); ?>
